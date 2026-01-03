8fe198dbc81fd6cdd3053473bbf045a5b6a3de59
Alli <alishbakhan6699@gmail.com>
2026-01-03 16:28:04 +0500

    initial commit

--- app/src/main/java/com/example/balishchat/MainActivity.java
+++ app/src/main/java/com/example/balishchat/MainActivity.java
@@ -18,7 +18,7 @@ import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
-
+4
 public class MainActivity extends AppCompatActivity {
 
     FirebaseAuth auth;
